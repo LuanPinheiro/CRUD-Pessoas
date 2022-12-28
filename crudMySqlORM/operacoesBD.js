80c3733fc9dc3d46f34f4e6c476c05ac27f41e39
LuanPinheiro <luanp.soledade@gmail.com>
2022-12-28 12:26:12 -0300

    CRUD com ORM com interação http
    Adicionado nova request no insomnia
    Operações organizadas
    Rotas funcionando de acordo
    Middlewares incompletos

--- crudMySqlORM/operacoesBD.js
+++ crudMySqlORM/operacoesBD.js
@@ -1,4 +1,54 @@
-(async ()=>{
+const database = require("./database");
+const dadosTabela = require("./pessoas");
+
+async function selecionarTabela(){ // Operação READ dos dados de uma tabela
+    const todasPessoas = await dadosTabela.findAll();
+    return todasPessoas; // Retorna para ser inserido no array
+}
+
+async function criarTabela(){
+    await database.sync();
+}
+
+async function inserirTabela(dados){ // Operação INSERT
+    const novaPessoa = await dadosTabela.create({
+        nome: `${dados.nome}`,
+        idade: `${dados.idade}`,
+        cpf: `${dados.cpf}`,
+        estaTrabalhando: `${dados.estaTrabalhando}`
+    })
+    return novaPessoa; // Retorna para ser inserido no array
+}
+
+async function editarElemento(elemento, dados){ // Operação UPDATE
+    const elementoAlterado = await dadosTabela.findByPk(elemento.id);
+    if(elementoAlterado){
+        elementoAlterado.nome = dados.nome;
+        elementoAlterado.idade = dados.idade;
+        elementoAlterado.cpf = dados.cpf;
+        elementoAlterado.estaTrabalhando = dados.estaTrabalhando;
+        await elementoAlterado.save(); // Salva a alteração feita no banco de dados
+    }
+    else{
+        console.log("Não Encontrado")
+    }
+    return elementoAlterado; // Retorna para o array
+}
+
+async function removerElemento(elemento){
+    if(elemento){
+        const elementoDeletado = await dadosTabela.findByPk(elemento.id);
+        await elementoDeletado.destroy();
+        return null;
+    }
+    else{
+        console.log("Nao Encontrado");
+        return elemento;
+    }
+}
+
+module.exports = {selecionarTabela, inserirTabela, criarTabela, editarElemento, removerElemento};
+/*async function  ()=>{
     const database = require('./database');
     const Pessoas = require('./pessoas');
     await database.sync(); // Cria as tabelas no banco de dados caso elas ainda não existam
@@ -33,4 +83,4 @@
     
     const todasPessoas = await Pessoas.findAll(); // operação READ
     console.log(todasPessoas); // Mostra no console todos os dados da tabela
-})();
+};*/
---
+++ crudMySqlORM/routes.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const operacoes = require("./operacoesBD");
+let dadosBD = null;
+const server = express();
+
+server.use(express.json()); // faz com que o express entenda JSON
+
+// Rota de criação de tabela
+server.get("/create", async(req, res) => {
+    await operacoes.criarTabela();
+    if(!dadosBD){
+        dadosBD = await operacoes.selecionarTabela();
+        return res.send("<h1>Banco sincronizado<h1>");
+    }
+    else{
+        return res.send("<h1>Banco já sincronizado anteriormente<h1>");
+    }
+})
+    
+
+// Rota de listagem dos dados
+server.get("/pessoas", async(req, res) => {
+    if(dadosBD){
+        dadosBD = await operacoes.selecionarTabela();
+        return res.json(dadosBD);
+    }
+    else{
+        return res.send(`<h1>Por favor inicie o banco na rota /create</h1>`);
+    }
+})
+
+// Criando um novo objeto no array
+server.post("/pessoas", checkPessoaValida, async(req, res) => {
+    dadosBD = await operacoes.selecionarTabela();
+    if(dadosBD){
+        dadosBD.push(await operacoes.inserirTabela(req.body));
+        return res.status(201).send(`<h1>Adicionado novo registro</h1>`); // Retorna o array com um novo elemento
+    }
+    else{
+        return res.send(`<h1>Por favor inicie o banco na rota /create</h1>`);
+    }
+})
+
+// Rota de listagem de 1 elemento do array
+server.get("/pessoas/:index", async(req, res) =>{
+    dadosBD = await operacoes.selecionarTabela();
+    if(dadosBD){
+        if(dadosBD[req.params.index]){
+            return res.json(dadosBD[req.params.index]); // Listando com base no parâmetro indicado na url
+        }
+        else{
+            return res.send(`<h1>Pessoa não encontrada</h1>`);
+        }
+        
+    }
+    else{
+        return res.send(`<h1>Por favor inicie o banco na rota /create</h1>`);
+    }
+})
+
+// Editar 1 elemento do array
+server.put('/pessoas/:index', checkPessoaValida, checkPessoaInArray, async(req, res) => {
+    dadosBD = await operacoes.selecionarTabela();
+    if(dadosBD){
+        if(dadosBD[req.params.index]){
+            dadosBD[req.params.index] = await operacoes.editarElemento(dadosBD[req.params.index], req.body);
+            return res.send(`<h1>Pessoa alterada</h1>`);
+        }
+        else{
+            return res.send(`<h1>Pessoa não encontrada</h1>`);
+        }
+    }
+    else{
+        return res.send(`<h1>Por favor inicie o banco na rota /create</h1>`);
+    }
+    
+});
+
+// Exclui um elemento do array
+server.delete('/pessoas/:index', checkPessoaInArray, async(req, res) => {
+    dadosBD = await operacoes.selecionarTabela();
+    if(dadosBD){
+        dadosBD[req.params.index] = await operacoes.removerElemento(dadosBD[req.params.index]);
+        dadosBD.splice(req.params.index,1);
+        return res.send(`<h1>Pessoa no índice ${req.params.index} apagada</h1>`);
+    }
+    else{
+        return res.send(`<h1>Por favor inicie o banco na rota /create</h1>`);
+    }
+});
+
+// MIDDLEWARES
+// Checa se as informações enviadas têm as chaves corretas
+function checkPessoaValida(req, res, next) { 
+    if (!req.body.nome || !req.body.idade || !req.body.cpf || (req.body.estaTrabalhando != false && req.body.estaTrabalhando != true)) {
+        return res.status(400).json({ error: 'Dados Incompletos' });
+    }
+    return next(); // Continua na rota em que essa função foi chamada caso não tenha encontrado erro
+}
+
+// Checa se há algum dado no index indicado
+function checkPessoaInArray(req, res, next) { 
+    if(isNaN(req.params.index) === true){ // Checando se o indice não é um numero, se não for retorna erro
+        return res.status(404).json({ error: 'Índice buscado nao é um numero'});
+    }
+    const pessoa = dadosBD[req.params.index]; // A pessoa a ser checada será indicada pelo index enviado por parametro na respectiva ação
+    if (!pessoa) {
+        return res.status(404).json({ error: 'Pessoa nao encontrada no array' });
+    }// checa se a pessoa existe no array, caso negativo informa que o index não existe no array
+
+    return next(); // Continua na rota em que essa função foi chamada caso não tenha encontrado erro
+}
+
+
+server.listen("3000", () => console.log("Server listening in port 3000"));
